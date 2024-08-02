c36653214fe5bbfff18a90802aefa156df442ee7
Kacper <kacperadamiak14@gmail.com>
2024-08-02 22:45:19 +0200

    Init

---
+++ src/main/java/com/sprint/boot/course/neverforgetbackend/NeverForgetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.boot.course.neverforgetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverForgetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverForgetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprint/boot/course/neverforgetbackend/NeverForgetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.boot.course.neverforgetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverForgetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
